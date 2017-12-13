ccfa247326fc689a1f685dd166bd9dc33a3bae41
Oleksandr Bogomaz <alex_bogomaz@ukr.net>
2017-12-13 16:10:22 +0200

    remove if netcoreapp2_0

--- src/Suave.Tests/WebSocket.fs
+++ src/Suave.Tests/WebSocket.fs
@@ -243,9 +243,6 @@ let websocketTests cfg =
         clientWebSocket.Close()
         Expect.isTrue (testByteArray (int PayloadSize.Bit32) echo.Value) "Should be echoed"
       
-      #if NETCOREAPP2_0
-      testCase "echo large number of messages to client" <| fun _ -> ()
-      #else      
       testCase websocketUrl subprotocols "echo large number of messages to client" <| fun mre clientWebSocket ->
         let amountOfMessages = 1000
         let echo = ref []
@@ -268,7 +265,6 @@ let websocketTests cfg =
 
         Expect.equal echo.Value expectedMessages "should be equal"
         Expect.equal (!count) amountOfMessages "received message count on websocket"
-      #endif
     ]
   
   testList "websocket tests" [ websocketTests websocketAppUrl [||]

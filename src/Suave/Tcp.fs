a45388c577d2bb208d5f6fd31b04b1c1a0b21ba3
Oleksandr Bogomaz <alex_bogomaz@ukr.net>
2018-01-23 13:36:14 +0200

    test commit

--- src/Suave/Tcp.fs
+++ src/Suave/Tcp.fs
@@ -150,7 +150,7 @@ extern int setsockopt(IntPtr socket, int level, int option_name, IntPtr option_v
 #nowarn "9"
 
 let enableRebinding (listenSocket: Socket) =
-  let mutable optionValue = 1
+  let mutable optionValue = 6
   //let setsockoptStatus = 0 
   //if RuntimeInformation.IsOSPlatform(OSPlatform.Linux) then 
   //nativeint
